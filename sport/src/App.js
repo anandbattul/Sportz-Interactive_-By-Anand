c9b3d3673e6890792357e01d7ead83b9aa6e5a70
ANAND BATTUL <92909928+anandbattul@users.noreply.github.com>
2022-03-31 17:17:41 +0530

    done

--- sport/src/App.js
+++ sport/src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CardList from './CardList';
+// import robots from './data';
+import SearchBox from './SearchBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+// function App() {
+//   return (
+//     <div className="tc">
+//       <h1>RoboProject</h1>
+//       <CardList robots={robots}/>
+//     </div>
+//   );
+// }
+
+//method 1
+
+//method 2
+
+//method 3
+
+class App extends React.Component{
+  
+  constructor(){
+    super();
+    this.state = {
+      // robots : robots,
+      robots : [],
+      searchField : '',
+    }
+
+    console.log('Constructor is running...')
+  }
+  
+
+  onInputChange = async(val) => {
+      await this.setState({searchField : val})
+      console.log(this.state.searchField);
+  }
+
+  // 54 < 65 < 78
+  // 65 > 45 > 34
+
+  // static getDerivedStateFromProps(){
+  //   console.log("getDerivedStateFromProps is running..")
+  // }
+
+  componentDidMount = () => {
+    // console.log("Component did mount is running...")
+    // await this.setState({robots : robots})
+    console.log("Component Did Mount is running");
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {
+          // console.log(users);
+          this.setState({robots : users})
+        })
+  }
+
+
+  render(){
+    console.log('Render is running...');
+
+    const {robots, searchField} = this.state;
+
+    const filteredRobots = robots.filter((robot) => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
+    if (!robots.length) {    // robots.length === 0  means we want to run
+      return <h1>Loading....</h1>
+    }
+     
+    return (
+      <div className="tc">
+        <h1>RoboProject</h1>
+        <SearchBox onInputChange={this.onInputChange}/> 
+        
+        <CardList robots={filteredRobots}/>
+      </div>
+    );
+  }
 }
+
+// class compoenntName extends React.Component{
+//   render(){
+//     return (
+
+//     )
+//   }
+// }
+
 
 export default App;
